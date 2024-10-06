99d73202e7ab8ae1c80b4a238413974c5efa2454
shahdmah <146766976+shahdmah@users.noreply.github.com>
2024-10-06 18:42:42 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.sendFile("./views/home.html" , {root: __dirname})
@@ -9,3 +10,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`)
 })
+
+mongoose.connect('mongodb+srv://shahdsalama:pkTBJP!iBtk5KV8/all-data@cluster0.uedxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+.then( () => { app.listen(port, () => {
+  console.log(`http://localhost:${port}/`)
+})})
+.catch( (err) => {console.log(err)});
